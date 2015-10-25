d2b7b5e6ea1d801fcd232cb98e31348141d8a020
Alejandro Wainzinger <aikawarazuni@gmail.com>
2015-10-25 17:11:15 +0900

    Move sales_price to user_item.

---
+++ db/migrate/20151025080100_move_sales_price_to_user_item.rb
@@ -0,0 +1,26 @@
+class MoveSalesPriceToUserItem < ActiveRecord::Migration
+  def up
+    add_column :user_items, :sales_price, :integer, null: false, default: 0
+    UserItem.reset_column_information
+
+    UserItem.includes(:user).each do |user_item|
+      user_item.sales_price = user.sales_price || 0
+      user_item.save!
+    end
+
+    remove_column :users, :sales_price
+  end
+
+  def down
+    add_column :users, :sales_price, :integer
+    User.reset_column_information
+
+    User.includes(:user_items).each do |user|
+      # Best-effort recovery. Grab the sales_price of the first if any.
+      user.sales_price = user_items.first.try(:sales_price)
+      user.save!
+    end
+
+    remove_column :user_items, :sales_price
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025075912) do
+ActiveRecord::Schema.define(version: 20151025080100) do
 
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,14 +21,14 @@ ActiveRecord::Schema.define(version: 20151025075912) do
   create_table "user_items", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "item_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "sales_price", default: 0, null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "sales_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
